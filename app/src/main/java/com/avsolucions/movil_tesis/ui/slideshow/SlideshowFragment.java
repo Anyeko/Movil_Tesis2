bad5473d190b61e48cb80934f1cdd170c16a0e9e
Anyeko <115649142+Anyeko@users.noreply.github.com>
2024-09-12 10:58:48 -0500

    Estructura de aplicacion movil

--- app/src/main/java/com/avsolucions/movil_tesis/ui/slideshow/SlideshowFragment.java
+++ app/src/main/java/com/avsolucions/movil_tesis/ui/slideshow/SlideshowFragment.java
@@ -2,17 +2,16 @@ package com.avsolucions.movil_tesis.ui.slideshow;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageButton;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
 
+import com.avsolucions.movil_tesis.MainActivity;
 import com.avsolucions.movil_tesis.R;
+import androidx.lifecycle.ViewModelProvider;
+import com.avsolucions.movil_tesis.databindig.FragmentSlideshowBinding;
 
 public class SlideshowFragment extends Fragment {
 
@@ -28,7 +27,7 @@ public class SlideshowFragment extends Fragment {
             @Override
             public void onClick(View v) {
                 // Redirigir a LoginActivity
-                Intent intent = new Intent(getActivity(), LoginActivity.class);
+                Intent intent = new Intent(getActivity(), MainActivity.class);
                 startActivity(intent);
             }
         });
